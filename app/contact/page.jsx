2111feda5744ebad1047e1b642bfb57b7b0171f0
SakiBee <hi.sakib75@gmail.com>
2024-08-20 02:21:28 +0600

    add button

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-// import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/Button";
 import {Input} from "@/components/ui/input";
 import {Textarea} from '@/components/ui/textarea';
 
